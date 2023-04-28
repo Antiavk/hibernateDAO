43a87df487b269abdf634c6fdd23db625af60e40
Antiavk <avk_tp@mail.ru>
2023-04-28 23:27:52 +0300

    Testing

---
+++ src/main/java/ru/netology/hibernate/entity/PersonId.java
@@ -0,0 +1,24 @@
+package ru.netology.hibernate.entity;
+
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+
+public class PersonId implements Serializable {
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surname;
+    @Column(nullable = false)
+    private int age;
+}
